104647e94bee5c91f9cf1a34be6760f030145328
Vinicius <vroliveira.eng@gmail.com>
2025-01-03 12:30:13 -0300

    Middlewares

---
+++ src/middlewares/manipuladorDeErros.js
@@ -0,0 +1,9 @@
+function manipuladorDeErros(erro, req, res, next) {
+        if (erro instanceof mongoose.Error.CastError) {
+          res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+        } else {
+          res.status(500).send({message: "Erro interno de servidor."});
+        }
+}
+
+export default manipuladorDeErros;
