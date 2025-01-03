22dee9b80eeeed1fc9789863129777c5605850dd
Vinicius <vroliveira.eng@gmail.com>
2025-01-03 12:00:31 -0300

    final fix

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    env: { node: true }
+  },
+  pluginJs.configs.recommended,
+];
