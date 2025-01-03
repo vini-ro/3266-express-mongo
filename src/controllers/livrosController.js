11101bbc11ba05710c4314055fc6bb27d8f1b004
Vinicius <vroliveira.eng@gmail.com>
2025-01-03 15:05:11 -0300

    Aplicando Regex na busca

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -77,11 +77,22 @@ class LivroController {
     }
   };
   
-  static listarLivroPorEditora = async (req, res, next) => {
+  static listarLivroPorFiltro = async (req, res, next) => {
     try {
-      const editora = req.query.editora;
+      const {editora, titulo} = req.query;
+
+      const regex = new RegExp(titulo, "i");
+
+      const busca = {};
+
+      if (editora) {
+        busca.editora = editora;
+      }
+      if (titulo) {
+        busca.titulo = {$regex: titulo, $options: "i"};
+      }
       
-      const livrosResultado = await livros.find({"editora": editora});
+      const livrosResultado = await livros.find(busca);
       
       res.status(200).send(livrosResultado);
     } catch (erro) {
